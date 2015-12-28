294db1331ff247534b75308d87bcae12b9efe36f
Daniel Wilkins <tekk@parlementum.net>
2015-12-28 18:31:30 -0500

    yay, components.

--- fenestral.js
+++ fenestral.js
@@ -4,7 +4,8 @@ function Fenestral(){
     var tileIdx = 0;
     this.setup = function()
     {
-        thingy = new jaws.Sprite({image: "dinosphere.png", x: 0, y: 0});
+        thingy = {components: {position: new cPosition(0, 0), drawable: new cDrawable("dinosphere.png")}};
+        componentClaim(thingy);
         maptiles = new jaws.SpriteSheet({image: "tileset.png", frame_size: [16, 16], scale_image: 2});
     },
     this.update = function()
@@ -17,8 +18,38 @@ function Fenestral(){
     this.draw = function()
     {
         jaws.clear();
-        thingy.draw();
+        componentDraw(thingy);
         currentTile.draw();
+    }
+
+
+    function componentDraw(o)
+    {
+        _.each(o.components, function(elm, idx, l)
+               { if (elm.draw) { elm.draw();} });
+    }
+
+    function componentClaim(o)
+    {
+        _.each(o.components, function(elm, idx, l)
+               { console.log("elm: " + elm); elm.owner = o });
+    }
+    function cPosition(x, y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    function cDrawable(sprite)
+    {
+        this.sprite = new jaws.Sprite({image: sprite});
+
+        this.draw = function()
+        {
+            this.sprite.x = this.owner.components.position.x;
+            this.sprite.y = this.owner.components.position.y;
+            this.sprite.draw();
+        }
     }
 }
 
