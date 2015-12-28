35f7ffd7bda747cd104de53c681d7ad1513a67c6
Daniel Wilkins <tekk@parlementum.net>
2015-12-28 18:35:28 -0500

    Fixed syntaxey stuff

--- fenestral.js
+++ fenestral.js
@@ -7,38 +7,40 @@ function Fenestral(){
         thingy = {components: {position: new cPosition(0, 0), drawable: new cDrawable("dinosphere.png")}};
         componentClaim(thingy);
         maptiles = new jaws.SpriteSheet({image: "tileset.png", frame_size: [16, 16], scale_image: 2});
-    },
+    };
+    
     this.update = function()
     {
         currentTile = new jaws.Sprite({image: maptiles.frames[tileIdx % maptiles.frames.length],
                                        x: 64, y: 64});
         tileIdx += 1;
-    },
+    };
     
     this.draw = function()
     {
         jaws.clear();
         componentDraw(thingy);
         currentTile.draw();
-    }
+    };
 
 
     function componentDraw(o)
     {
         _.each(o.components, function(elm, idx, l)
                { if (elm.draw) { elm.draw();} });
-    }
+    };
 
     function componentClaim(o)
     {
         _.each(o.components, function(elm, idx, l)
                { console.log("elm: " + elm); elm.owner = o });
-    }
+    };
+    
     function cPosition(x, y)
     {
         this.x = x;
         this.y = y;
-    }
+    };
 
     function cDrawable(sprite)
     {
@@ -50,7 +52,7 @@ function Fenestral(){
             this.sprite.y = this.owner.components.position.y;
             this.sprite.draw();
         }
-    }
+    };
 }
 
 
