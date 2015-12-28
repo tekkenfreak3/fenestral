a57430d8a16ff70e691b2128ad108d61c7b3612a
Daniel Wilkins <tekk@parlementum.net>
2015-12-28 17:18:24 -0500

    Tileset loading.

--- fenestral.js
+++ fenestral.js
@@ -1,7 +1,25 @@
 function Fenestral(){
     var thingy;
-    this.setup = function() {thingy = new jaws.Sprite({image: "dinosphere.png", x: 0, y: 0});},
-    this.draw = function() { jaws.clear(); thingy.draw();}
+    var maptiles;
+    var tileIdx = 0;
+    this.setup = function()
+    {
+        thingy = new jaws.Sprite({image: "dinosphere.png", x: 0, y: 0});
+        maptiles = new jaws.SpriteSheet({image: "tileset.png", frame_size: [16, 16]});
+    },
+    this.update = function()
+    {
+        currentTile = new jaws.Sprite({image: maptiles.frames[tileIdx % maptiles.frames.length],
+                                       x: 64, y: 64});
+        tileIdx += 1;
+    },
+    
+    this.draw = function()
+    {
+        jaws.clear();
+        thingy.draw();
+        currentTile.draw();
+    }
 }
 
 
